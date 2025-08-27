a210206819c9160764c5fc348d8872ad024f9610
Aymeric PINEAU <ay.pineau@gmail.com>
2025-08-27 21:32:49 +0200

    refactor: remove unused docHeader.content.ts and simplify DocHeader component structure

--- website/src/components/DocPage/DocHeader/DocHeader.tsx
+++ website/src/components/DocPage/DocHeader/DocHeader.tsx
@@ -1,9 +1,7 @@
 import { Container } from "@components/Container";
 import { DocMetadata } from "@smart-doc/docs";
 import { Locales } from "intlayer";
-import { useIntlayer } from "next-intlayer/server";
 import { FC } from "react";
-import { ApplicationTemplateMessage } from "../ApplicationTemplateMessage";
 import { ContributionMessage } from "../ContributionMessage";
 import { CopyMarkdownMessage } from "../CopyMarkdownMessage";
 import { SummarizeAI } from "../SummarizeAI/SummarizeAI";
@@ -19,28 +17,23 @@ export const DocHeader: FC<DocHeaderProps> = ({
   markdownContent,
   githubUrl,
   locale,
-}) => {
-  const { authorLabel, creationLabel, lastUpdateLabel } =
-    useIntlayer("doc-header");
-
-  return (
-    <>
-      <Container className="sticky top-20 mt-5 z-5 flex flex-col gap-2 px-4 py-2 max-w-[95%] mx-auto">
-        <div className="flex flex-row gap-4 w-full justify-between">
-          <div className="flex flex-row gap-4 w-full justify-start items-center">
-            <SummarizeAI url={`${process.env.NEXT_PUBLIC_URL}${relativeUrl}`} />
-          </div>
-          <div className="flex flex-row gap-4 w-full justify-end items-center">
-            <TranslatedContentMessage pageUrl={relativeUrl} />
-
-            <ContributionMessage
-              githubUrl={githubUrl.replace("/en/", `/${locale}/`)}
-            />
-
-            <CopyMarkdownMessage markdownContent={markdownContent} />
-          </div>
+}) => (
+  <>
+    <Container className="sticky top-20 mt-5 z-5 flex flex-col gap-2 px-4 py-2 max-w-[95%] mx-auto">
+      <div className="flex flex-row gap-4 w-full justify-between">
+        <div className="flex flex-row gap-4 w-full justify-start items-center">
+          <SummarizeAI url={`${process.env.NEXT_PUBLIC_URL}${relativeUrl}`} />
         </div>
-      </Container>
-    </>
-  );
-};
+        <div className="flex flex-row gap-4 w-full justify-end items-center">
+          <TranslatedContentMessage pageUrl={relativeUrl} />
+
+          <ContributionMessage
+            githubUrl={githubUrl.replace("/en/", `/${locale}/`)}
+          />
+
+          <CopyMarkdownMessage markdownContent={markdownContent} />
+        </div>
+      </div>
+    </Container>
+  </>
+);
